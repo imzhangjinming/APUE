0f5a541405678159b8b6e1fd4ed847c1001c2576
imzhangjinming <208559590@qq.com>
2021-08-28 02:26:51 +0800

    correct bit_or_little_endian.c

--- 16/exercise/big_or_little_endian.c
+++ 16/exercise/big_or_little_endian.c
@@ -4,9 +4,9 @@ int main(void){
 	int a = 0x04030201;
 
 	if(*(char*)(&a)==0x01)
-		printf("big endian\n");
-	else if(*(char*)(&a)==0x04)
 		printf("little endian\n");
+	else if(*(char*)(&a)==0x04)
+		printf("big endian\n");
 	else
 		printf("can't determine\n");
 }
